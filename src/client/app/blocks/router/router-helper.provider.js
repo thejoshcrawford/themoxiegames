dca29dc1466f15b843951679ab4cad8058373e0f
Josh Crawford <josh@crawford.cc>
2016-01-18 21:15:16 -0800

    fixed html5mode

--- src/client/app/blocks/router/router-helper.provider.js
+++ src/client/app/blocks/router/router-helper.provider.js
@@ -15,7 +15,7 @@
             resolveAlways: {}
         };
 
-        $locationProvider.html5Mode(true);
+        //$locationProvider.html5Mode(true);
 
         this.configure = function(cfg) {
             angular.extend(config, cfg);
@@ -38,8 +38,6 @@
                 stateCounts: stateCounts
             };
 
-            init();
-
             return service;
 
             ///////////////
@@ -55,47 +53,8 @@
                     $urlRouterProvider.otherwise(otherwisePath);
                 }
             }
-
-            function handleRoutingErrors() {
-                // Route cancellation:
-                // On routing error, go to the dashboard.
-                // Provide an exit clause if it tries to do it twice.
-                $rootScope.$on('$stateChangeError',
-                    function(event, toState, toParams, fromState, fromParams, error) {
-                        if (handlingStateChangeError) {
-                            return;
-                        }
-                        stateCounts.errors++;
-                        handlingStateChangeError = true;
-                        var destination = (toState &&
-                            (toState.title || toState.name || toState.loadedTemplateUrl)) ||
-                            'unknown target';
-                        var msg = 'Error routing to ' + destination + '. ' +
-                            (error.data || '') + '. <br/>' + (error.statusText || '') +
-                            ': ' + (error.status || '');
-                        logger.warning(msg, [toState]);
-                        $location.path('/');
-                    }
-                );
-            }
-
-            function init() {
-                handleRoutingErrors();
-                updateDocTitle();
-            }
-
+            
             function getStates() { return $state.get(); }
-
-            function updateDocTitle() {
-                $rootScope.$on('$stateChangeSuccess',
-                    function(event, toState, toParams, fromState, fromParams) {
-                        stateCounts.changes++;
-                        handlingStateChangeError = false;
-                        var title = config.docTitle + ' ' + (toState.title || '');
-                        $rootScope.title = title; // data bind to <title>
-                    }
-                );
-            }
         }
     }
 })();
